28c417d8f8d1eef6383e9f655273af5322389941
Siva Priyan <sprverse@gmail.com>
2024-03-28 21:38:14 -0400

    finial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IComponent<string> component = new ConcreteComponent();
+        IComponent<string> plainDecorator = new PlainDecorator(component);
+        IComponent<string> upperCaseDecorator = new UpperCaseDecorator(component);
+        IComponent<string> colorDecorator = new ColorDecorator(component);
+
+        Console.WriteLine(component.GetText());
+        Console.WriteLine(plainDecorator.GetText());
+        Console.WriteLine(upperCaseDecorator.GetText());
+        Console.WriteLine(colorDecorator.GetText());
+    }
+}
